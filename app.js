8ef876dc3dd8a57e5b12f980a3f1048e16f5e999
Laura Serbêto <115957780+lauraserbeto@users.noreply.github.com>
2024-10-03 15:27:58 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+alert('Boas vindas ao jogo do número secreto!');
+
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1
+
+
+//enquanto o chute não for igual ao n° secreto
+while(chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 100');
+// se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if(chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1
+        tentativas++;
+    }
+}
+
+/*if(tentativas > 1){
+    alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto} com ${tentativas} tentativas) `);
+} else {
+    alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto} com ${tentativas} tentativa) `);
+}*/
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} ${palavraTentativa} `);
+
+
+
